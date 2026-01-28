f87895e911b661c5cfd4416cdba3295a545d87cf
unknown <asazycat@gmail.com>
2026-01-28 10:02:41 +0000

    env

--- src/db.ts
+++ src/db.ts
@@ -2,13 +2,11 @@ import mysql, { Connection } from 'mysql2/promise';
 
 export const db: Promise<Connection> = (async () =>  {
   try{ return await mysql.createConnection({
-    host:'localhost',
-    user: 'root',
-    password:'kiloloki',
-    database:'portfolio',
-    port:3306
-
-}).then((res) => res)
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
+}).then((res: any) => res)
   } catch(err: any) {
     console.log(`${err.statusCode}, ${err.message} ---------------`)
     return err
