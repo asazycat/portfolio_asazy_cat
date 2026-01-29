7fb4be130a84e8bfff979e2605203ba613ea8009
unknown <asazycat@gmail.com>
2026-01-29 17:18:37 +0000

    projects

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,12 +2,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const nav = document.getElementsByTagName('nav')[0];
     const burgerButton = document.getElementById('burgerMenu');
-    console.log(nav)
     burgerButton.addEventListener('click', () => {
         nav.style.display === 'block' ? nav.style.display = 'none' : nav.style.display = 'block'
     })
     window.addEventListener('resize', () => {
-        console.log('size')
         if(window.innerWidth >= 1000) {
             nav.style.display = 'flex';
             nav.style.flexDirection = 'row';
